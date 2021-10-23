e8eb153eefa33d591c69a147778c5f7070d4f528
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-10-23 15:55:14 +0800

    修復 網頁*3
    yes,  *4 coming soon

--- Source/dashboard/app.js
+++ Source/dashboard/app.js
@@ -34,7 +34,7 @@ module.exports.load = async client => {
     // Set the ejs templates to ./views
     .set('views', path.join(__dirname, '/views'))
     // Set the dashboard port
-    .set('port', process.env.DashboardPort || 3000)
+    .set('port', process.env.PORT || 3000)
     // Set the express session password and configuration
     .use(
       session({
