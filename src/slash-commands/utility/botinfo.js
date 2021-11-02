52bfefc35baa0fb235642597abac5df3bd9cca83
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-11-02 19:53:28 +0800

    v3.2.2b

--- src/slash-commands/utility/botinfo.js
+++ src/slash-commands/utility/botinfo.js
@@ -21,34 +21,34 @@ module.exports = {
       .setAuthor(`${bot.user.username}的資訊`, bot.user.displayAvatarURL())
       .addField(
         '__**一般介紹**__',
-        `**Bot Id:** ${bot.user.id}
-**Bot Tag:** ${bot.user.tag}
-**創建於 :** <t:${createdAt}:F>
-**創建者: [CC_#8844](https:www.youtube.com)**`
+        `**Bot Id：** ${bot.user.id}
+**Bot Tag：** ${bot.user.tag}
+**創建於 ：** <t:${createdAt}:F>
+**創建者： [CC_#8844](https:\/\/www.youtube.com)**`
       )
       .addField(
-        '__**狀態:**__',
-        `**用戶:** ${util.formatNumber(users)}
-**伺服器:** ${util.formatNumber(bot.guilds.cache.size)}
-**頻道:** ${util.formatNumber(bot.channels.cache.size)}
-**指令數:** ${util.formatNumber(bot.slashCommands.size)}`
+        '__**狀態：**__',
+        `**用戶：** ${util.formatNumber(users)}
+**伺服器：** ${util.formatNumber(bot.guilds.cache.size)}
+**頻道：** ${util.formatNumber(bot.channels.cache.size)}
+**指令數：** ${util.formatNumber(bot.slashCommands.size)}`
       )
       .addField(
         '__**系統介紹**__',
-        `**已使用 記憶體:**  ${(
+        `**已使用 記憶體：**  ${(
           process.memoryUsage().heapUsed /
           1024 /
           1024
         ).toFixed(2)} MB
-**記憶體總數:** ${(os.totalmem() / 1024 / 1024 / 1024).toFixed(2)} GB
-**已上線時間:** ${uptime}
-**Node 版本:** ${nodev}
-**Discord.js 版本:** ${version}
-**CPU:** ${core.model}
-**核心數:** ${os.cpus().length}
-**速度:** ${core.speed} MHz
-**位元:** ${os.arch()}
-**作業系統:** ${util.toCapitalize(process.platform)}`
+**記憶體總數：** ${(os.totalmem() / 1024 / 1024 / 1024).toFixed(2)} GB
+**已上線時間：** ${uptime}
+**Node 版本：** ${nodev}
+**Discord.js 版本：** ${version}
+**CPU：** ${core.model}
+**核心數：** ${os.cpus().length}
+**速度：** ${core.speed} MHz
+**位元：** ${os.arch()}
+**作業系統：** ${util.toCapitalize(process.platform)}`
       )
     return interaction.reply({
       ephemeral: true,
