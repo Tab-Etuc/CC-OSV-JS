b66bb7f2311482b827aa476973703f108bc1b5f7
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-11-08 20:50:18 +0800

    Update CangeChannelTime.js

--- src/task/CangeChannelTime.js
+++ src/task/CangeChannelTime.js
@@ -6,7 +6,7 @@ const data_date = ['852364573095755808']
 module.exports = async function ChangeTime (bot) {
   setInterval(() => {
     try {
-      let channels = bot.channels.cache
+      let channels = bot.channels
       console.log(channels)
       // Time
       for (i in channels) {
