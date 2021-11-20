a46d0e99ea887c10c0af4e19eb3232aff7fa1889
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-11-20 18:56:56 +0800

    v3.3.5

--- src/commands/slash/economy/balance.js
+++ src/commands/slash/economy/balance.js
@@ -1,23 +1,47 @@
-const { MessageEmbed } = require('discord.js');
-module.exports = {
-    name: "餘額",
-    description: "查看您的餘額。",
-    category: "經濟",
-    options: [{
-        name: '玩家',
-        type: 'USER',
-        description: '您欲搜尋之用戶個人資料(註：選填。)',
-        required: false,
-    }],
-    async execute(bot, interaction) {
-        const member = interaction.options.getString("指令", false) || interaction.member;
+const { MessageEmbed } = require('discord.js')
+const Users = require('../../../models/mongoDB/Users')
 
-        const user = await bot.fetchUser(bot, member.id);
-        const embed = new MessageEmbed()
-            .setTitle(`${member.user.username}'s Balance`)
-            .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-            .setDescription(`💳**Wallet**: ${user.coinsInWallet.toLocaleString()}\n🏦**Bank**: ${user.coinsInBank.toLocaleString()}/${user.bankSpace.toLocaleString()}\n🌐**Total Net Worth**: ${(user.coinsInWallet + user.coinsInBank).toLocaleString()}`)
-            .setColor('RANDOM');
-        interaction.reply({embeds:[embed]});
+module.exports = {
+  name: '餘額',
+  description: '查看您的餘額。',
+  category: '經濟',
+  options: [
+    {
+      name: '玩家',
+      type: 'USER',
+      description: '您欲搜尋之用戶個人資料(註：選填。)',
+      required: false
     }
+  ],
+  async execute (bot, interaction) {
+    const member =
+      interaction.options.getString('指令', false) || interaction.member
+
+    const someone = bot.users.cache.get(member.id)
+
+    let user = await Users.findOne({
+      guildId: interaction.guild.id,
+      userId: member.id
+    })
+    console.log(member.id)
+    if (!user) {
+      const newUser = new Users({
+        userId: member.id,
+        guildId: interaction.guild.id,
+        userName: someone.username
+      }).save()
+      user = newUser
+    }
+
+    const embed = new MessageEmbed()
+      .setTitle(`${member.user.username}'s Balance`)
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+      .setDescription(
+        `💳**Wallet**: ${user.coinsInWallet.toLocaleString()}\n🏦**Bank**: ${user.coinsInBank.toLocaleString()}/${user.bankSpace.toLocaleString()}\n🌐**Total Net Worth**: ${(
+          user.coinsInWallet + user.coinsInBank
+        ).toLocaleString()}`
+      )
+      .setColor('RANDOM')
+    interaction.reply({ embeds: [embed] })
+  }
 }
