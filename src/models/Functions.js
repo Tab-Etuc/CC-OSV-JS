7ea52ef8fc722cdd734927ea17e5bfab68337ad2
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-11-25 20:06:28 +0800

    v3.3.6

--- src/models/Functions.js
+++ src/models/Functions.js
@@ -1,187 +1,3 @@
-const DJS = require('discord.js')
-async function getMember (message, toFind = '') {
-  toFind = toFind.toLowerCase()
-
-  let target = message.guild.members.get(toFind)
-
-  if (!target && message.mentions.members)
-    target = message.mentions.members.first()
-
-  if (!target && toFind) {
-    target = message.guild.members.find(member => {
-      return (
-        member.displayName.toLowerCase().includes(toFind) ||
-        member.user.tag.toLowerCase().includes(toFind)
-      )
-    })
-  }
-
-  if (!target) target = message.member
-
-  return target
-}
-
-async function formatDate (date) {
-  return new Intl.DateTimeFormat('en-US').format(date)
-}
-
-async function promptMessage (message, author, time, validReactions) {
-  // We put in the time as seconds, with this it's being transfered to MS
-  time *= 1000
-
-  // For every emoji in the function parameters, react in the good order.
-  for (const reaction of validReactions) await message.react(reaction)
-
-  // Only allow reactions from the author,
-  // and the emoji must be in the array we provided.
-  const filter = (reaction, user) =>
-    validReactions.includes(reaction.emoji.name) && user.id === author.id
-
-  // And ofcourse, await the reactions
-  return message
-    .awaitReactions(filter, { max: 1, time: time })
-    .then(collected => collected.first() && collected.first().emoji.name)
-}
-
-async function forHumans (o) {
-  for (
-    var r = [
-        [Math.floor(o / 31536e3), '年'],
-        [Math.floor((o % 31536e3) / 86400), '日'],
-        [Math.floor(((o % 31536e3) % 86400) / 3600), '小時'],
-        [Math.floor((((o % 31536e3) % 86400) % 3600) / 60), '分鐘'],
-        [(((o % 31536e3) % 86400) % 3600) % 60, '秒']
-      ],
-      e = '',
-      t = 0,
-      s = r.length;
-    t < s;
-    t++
-  )
-    0 !== r[t][0] &&
-      (e +=
-        ' ' +
-        r[t][0] +
-        ' ' +
-        (1 === r[t][0] ? r[t][1].substr(0, r[t][1].length - 1) : r[t][1]))
-  return e.trim()
-}
-async function sendErrorLog (bot, error, type) {
-  try {
-    if (
-      error.message?.includes('Missing Access') ||
-      error.message?.includes('Missing Permissions')
-    )
-      return
-
-    if (
-      error.stack?.includes?.(
-        'DeprecationWarning: Listening to events on the Db class'
-      )
-    )
-      return
-
-    const channelId = '851788198467338242'
-    if (!channelId) {
-      return bot.logger.error('錯誤日誌', error?.stack || `${error}`)
-    }
-
-    const channel =
-      bot.channels.cache.get(channelId) || (await bot.channels.fetch(channelId))
-
-    if (!channel || !havePermissions(channel)) {
-      return bot.logger.error('錯誤日誌', error?.stack || `${error}`)
-    }
-
-    const webhooks = await channel.fetchWebhooks()
-    const hook = webhooks.first()
-
-    if (!hook) {
-      return bot.logger.error('錯誤日誌', error?.stack || `${error}`)
-    }
-
-    const code = error.code || '無'
-    const httpStatus = error.httpStatus || '無'
-    const requestData = error.requestData ?? { json: {} }
-    const name = error.name || '無'
-    let stack = error.stack || error
-    let jsonString
-
-    try {
-      jsonString = JSON.stringify(requestData.json, null, 2)
-    } catch {
-      jsonString = ''
-    }
-
-    if (jsonString?.length >= 4096) {
-      jsonString = jsonString ? `${jsonString?.substr(0, 4090)}...` : ''
-    }
-
-    if (typeof stack === 'object') stack = JSON.stringify(stack)
-
-    if (typeof stack === 'string' && stack.length >= 4096) {
-      console.error(stack)
-      stack =
-        'An error occurred but was too long to send to Discord, check your console.'
-    }
-
-    const { codeBlock } = require('@discordjs/builders')
-
-    const embed = new DJS.MessageEmbed()
-      .setTitle('錯誤日誌')
-      .addField('名稱', name, true)
-      .addField('程式', code.toString(), true)
-      .addField('HTTP狀態', httpStatus.toString(), true)
-      .addField('時間點', bot.logger.now, true)
-      .addField('請求數據', codeBlock(jsonString?.substr(0, 1020)))
-      .setDescription(`${codeBlock(stack)}`)
-      .setColor(type === 'error' ? 'RED' : 'ORANGE')
-
-    await hook.send({ embeds: [embed] })
-  } catch (e) {
-    console.error({ error })
-    console.error(e)
-  }
-}
-
-/**
- * Check if the bot has the default permissions
- * @param {DJS.Interaction | DJS.TextChannel} resolveable
- * @returns {boolean}
- */
-function havePermissions (resolveable) {
-  const ch = 'channel' in resolveable ? resolveable.channel : resolveable
-  if (ch instanceof DJS.ThreadChannel || ch instanceof DJS.DMChannel)
-    return true
-  return (
-    ch
-      .permissionsFor(resolveable.guild.me)
-      ?.has(DJS.Permissions.FLAGS.VIEW_CHANNEL) &&
-    ch
-      .permissionsFor(resolveable.guild.me)
-      ?.has(DJS.Permissions.FLAGS.SEND_MESSAGES) &&
-    ch
-      .permissionsFor(resolveable.guild.me)
-      ?.has(DJS.Permissions.FLAGS.EMBED_LINKS)
-  )
-}
-
-/**
- * @param {string} str
- * @returns {string}
- */
-function toCapitalize (str) {
-  if (str === null || str === '') {
-    return false
-  } else {
-    str = str.toString()
-  }
-
-  return str.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-  })
-}
-
 /**
  * @param {number | string} n
  * @returns {string}
@@ -189,7 +5,6 @@ function toCapitalize (str) {
 function formatNumber (n) {
   return Number.parseFloat(String(n)).toLocaleString('be-BE')
 }
-
 /**
  * @param {number} int
  * @returns {string}
@@ -197,7 +12,6 @@ function formatNumber (n) {
 function formatInt (int) {
   return int < 10 ? `0${int}` : int
 }
-
 /**
  * Format duration to string
  * @param {number} millisec Duration in milliseconds
@@ -213,7 +27,6 @@ function formatDuration (millisec) {
   if (minutes > 0) return `${formatInt(minutes)}:${formatInt(seconds)}`
   return `00:${formatInt(seconds)}`
 }
-
 /**
  * Convert formatted duration to seconds
  * @param {string} formatted duration input
@@ -235,7 +48,6 @@ function toMilliSeconds (input) {
     return Number(input.replace(/[^\d.]+/g, '') * 1000) || 0
   }
 }
-
 /**
  * Parse number from input
  * @param {*} input Any
@@ -245,109 +57,6 @@ function parseNumber (input) {
   if (typeof input === 'string')
     return Number(input.replace(/[^\d.]+/g, '')) || 0
   return Number(input) || 0
-}
-
-/**
- * @param {string} string
- * @returns {string}
- */
-function codeContent (string, extension = '') {
-  return `\`\`\`${extension}\n${string}\`\`\``
-}
-
-/**
- * Check if modify queue
- * @param {Interaction} interaction
- * @returns {boolean}
- */
-function canModifyQueue (interaction) {
-  const memberChannelId = interaction.member.voice.channelId
-  const botChannelId = interaction.guild.me.voice.channelId
-
-  if (!memberChannelId) {
-    const embed1 = new DJS.MessageEmbed()
-      .setDescription('You need to join a voice channel first!')
-      .setColor('ORANGE')
-
-    return interaction
-      .reply({
-        ephemeral: true,
-        embeds: [embed1],
-        allowedMentions: { repliedUser: false }
-      })
-      .catch(console.error)
-  }
-
-  if (memberChannelId !== botChannelId) {
-    const embed2 = new DJS.MessageEmbed()
-      .setDescription('You must be in the same voice channel as me!')
-      .setColor('ORANGE')
-
-    return interaction
-      .reply({
-        ephemeral: true,
-        embeds: [embed2],
-        allowedMentions: { repliedUser: false }
-      })
-      .catch(console.error)
-  }
-
-  return true
-}
-/**
- * 轉全形字元
- */
-function toDBC (str) {
-  var result = ''
-  var len = str.length
-  for (var i = 0; i < len; i) {
-    var cCode = str.charCodeAt(i)
-    //全形與半形相差（除空格外）：65248(十進位制)
-    cCode = cCode >= 0x0021 && cCode <= 0x007e ? cCode + 65248 : cCode
-    //處理空格
-    cCode = cCode == 0x0020 ? 0x03000 : cCode
-    result = String.fromCharCode(cCode)
-  }
-  return result
-}
-/**
- * 轉半形字元
- */
-function toSBC (str) {
-  var result = ''
-  for (var i = 0, len = str.length; i < len; i) {
-    var cCode = str.charCodeAt(i)
-    //全形與半形相差（除空格外）：65248（十進位制）
-    cCode = cCode >= 0xff01 && cCode <= 0xff5e ? cCode - 65248 : cCode
-    //處理空格
-    cCode = cCode == 0x03000 ? 0x0020 : cCode
-    result = String.fromCharCode(cCode)
-  }
-  return result
-}
-
-function get_substrings_between (str, startDelimiter, endDelimiter) {
-  var contents = []
-  var startDelimiterLength = startDelimiter.length
-  var endDelimiterLength = endDelimiter.length
-  var startFrom = (contentStart = contentEnd = 0)
-
-  while (false !== (contentStart = strpos(str, startDelimiter, startFrom))) {
-    contentStart += startDelimiterLength
-    contentEnd = strpos(str, endDelimiter, contentStart)
-    if (false === contentEnd) {
-      break
-    }
-    contents.push(str.substr(contentStart, contentEnd - contentStart))
-    startFrom = contentEnd + endDelimiterLength
-  }
-
-  return contents
-}
-
-function strpos (haystack, needle, offset) {
-  var i = (haystack + '').indexOf(needle, offset || 0)
-  return i === -1 ? false : i
 }
 
 function numberFormat (num) {
@@ -371,23 +80,10 @@ function numberFormat (num) {
   )
 }
 module.exports = {
-  getMember,
-  formatDate,
-  promptMessage,
-  forHumans,
-  sendErrorLog,
-  havePermissions,
-  toCapitalize,
   formatNumber,
   formatInt,
   formatDuration,
   toMilliSeconds,
   parseNumber,
-  codeContent,
-  canModifyQueue,
-  toDBC,
-  toSBC,
-  get_substrings_between,
-  strpos,
   numberFormat
 }
