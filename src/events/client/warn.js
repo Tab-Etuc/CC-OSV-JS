aafa8028f0ad78f79ddc47d28a097ebb57eb59a8
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-11-25 20:06:03 +0800

    v3.3.6

--- src/events/client/warn.js
+++ src/events/client/warn.js
@@ -2,6 +2,6 @@ module.exports = {
   name: "warn",
   once: false,
   execute(bot, error) {
-    return bot.utils.sendErrorLog(bot, error, "warning");
+    return bot.logger.sendErrorLog(bot, error, "warning");
   }
 };
