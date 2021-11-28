fcab346823ecbcae1fd2659d56dd3fde538895b9
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-11-28 13:48:34 +0800

    v3.3.8

--- src/commands/slash/economy/withdraw.js
+++ src/commands/slash/economy/withdraw.js
@@ -11,28 +11,30 @@ module.exports = {
         required: true,
     }],
     async execute(bot, interaction) {
+    await interaction.deferReply()
+
         let data = await UserManager.fetchUser(bot, interaction.member.id, interaction.guild.id);
         const arg = interaction.options.getString("金額", false);
         if (arg.toLowerCase() === 'all' || arg === '全部' || arg === '全' || arg === '所有' || arg === 'a' || arg === 'ＡＬＬ' || arg === 'ａｌｌ' || arg === 'ａ') {
             data.coinsInWallet += data.coinsInBank;
 
-            await interaction.reply(`已取出 \`${data.coinsInBank}\` 元！`);
+            await interaction.editReply(`已取出 \`${data.coinsInBank}\` 元！`);
 
             data.coinsInBank -= data.coinsInBank;
 
             await data.save();
         } else {
             if (isNaN(arg)) {
-                return interaction.reply('請您輸入一個正確的數字。');
+                return interaction.editReply('請您輸入一個正確的數字。');
             }
 
             if (parseInt(arg) > data.coinsInBank) {
-                return interaction.reply('您的銀行存額不足，請提升。');
+                return interaction.editReply('您的銀行存額不足，請提升。');
             }
 
             data.coinsInWallet += parseInt(arg);
 
-            await interaction.reply(`已取出 \`${arg}\` 元！`);
+            await interaction.editReply(`已取出 \`${arg}\` 元！`);
 
             data.coinsInBank -= parseInt(arg);
 
