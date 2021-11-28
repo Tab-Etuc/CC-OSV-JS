229c0a142d7091604085c7b94cd7dc98845c4be3
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-11-28 13:48:21 +0800

    v3.3.8

--- src/commands/slash/economy/deposit.js
+++ src/commands/slash/economy/deposit.js
@@ -14,6 +14,8 @@ module.exports = {
     }
   ],
   async execute (bot, interaction) {
+    await interaction.deferReply()
+
     let data = await UserManager.fetchUser(bot, interaction.member.id, interaction.guild.id)
     const arg = interaction.options.getString('金額', false)
     if (
@@ -32,7 +34,7 @@ module.exports = {
 
         data.coinsInWallet = max_deposit
 
-        await interaction.reply(
+        await interaction.editReply(
           `已存入 \`${data.bankSpace - data.coinsInBank}\` 元！`
         )
 
@@ -43,7 +45,7 @@ module.exports = {
         if (data.coinsInWallet + data.coinsInBank > data.bankSpace) {
           const left = data.coinsInWallet + data.coinsInBank - data.bankSpace
 
-          interaction.reply(
+          interaction.editReply(
             `已存入 \`${(data.coinsInWallet - left).toLocaleString()}\` 元！`
           )
 
@@ -52,7 +54,7 @@ module.exports = {
 
           await data.save()
         } else {
-          interaction.reply(
+          interaction.editReply(
             `已存入 \`${data.coinsInWallet.toLocaleString()}\` 元！`
           )
 
@@ -64,22 +66,22 @@ module.exports = {
       }
     } else {
       if (isNaN(arg)) {
-        return interaction.reply('請您輸入一個正確的數字。')
+        return interaction.editReply('請您輸入一個正確的數字。')
       }
 
       if (parseInt(arg) > data.bankSpace) {
-        return interaction.reply('您的銀行存額不足，請提升。')
+        return interaction.editReply('您的銀行存額不足，請提升。')
       }
       if (parseInt(arg) > data.coinsInWallet) {
-        return interaction.reply('您沒有這麼多錢。')
+        return interaction.editReply('您沒有這麼多錢。')
       }
       if (parseInt(arg) > data.bankSpace - data.coinsInBank) {
-        return interaction.reply('您的銀行存額不足，請提升。')
+        return interaction.editReply('您的銀行存額不足，請提升。')
       }
 
       data.coinsInBank += parseInt(args[0])
 
-      await interaction.reply(`已存入 \`${args[0]}\` 元！`)
+      await interaction.editReply(`已存入 \`${args[0]}\` 元！`)
 
       data.coinsInWallet -= parseInt(args[0])
 
