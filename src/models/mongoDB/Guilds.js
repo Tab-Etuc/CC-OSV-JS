c32e251f964af50993cc3c99ef0138dff21451aa
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-11-30 20:15:54 +0800

    v3.4.0

--- src/models/mongoDB/Guilds.js
+++ src/models/mongoDB/Guilds.js
@@ -4,8 +4,8 @@ const guldSchema = mongoose.Schema({
   _id: { type: String, required: false },
   prefix: { type: String, required: false, default: 'C' },
   Language: { type: String, required: false, default: 'zh-TW' },
-  CommandsRan: 0,
-  SongsPlayed: 0,
+  CommandsRan: { type: Number, required: false, default: 0 },
+  SongsPlayed: { type: Number, required: false, default: 0 },
   ClockTime: { type: Array, required: false, default: [] },
   ClockDate: { type: Array, required: false, default: [] },
   EmojiRole: { type: Array, required: false, default: {} }
