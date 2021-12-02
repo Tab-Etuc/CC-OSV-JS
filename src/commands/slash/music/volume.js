d7974d5eb61fe91b1fe6535de2bd5402fa81a495
Tab-Etuc <xji.gl4.vu@gmail.com>
2021-12-02 20:23:56 +0800

    v3.4.0

--- src/commands/slash/music/volume.js
+++ src/commands/slash/music/volume.js
@@ -31,7 +31,7 @@ module.exports = {
     if (!player)
       return bot.say.slashError(
         interaction,
-        '❌ | **目前沒有播放任何音樂...**'
+        '**目前沒有播放任何音樂...**'
       )
     if (!arg)
       return bot.say.slashInfo(
