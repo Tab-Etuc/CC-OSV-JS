4084b0d693a5597abd4a93506c875cc3e18fb138
Tab Etuc <83118946+Tab-Etuc@users.noreply.github.com>
2022-12-11 22:43:49 +0800

    Update ping.ts

--- src/commands/slash/info/ping.ts
+++ src/commands/slash/info/ping.ts
@@ -2,21 +2,18 @@ import { snowflakeToTimestamp } from "@utils/snowflake.ts";
 import { addSlashCommand } from "@classes/command.ts";
 import { CCOSVEmbed } from "@classes/embed.ts";
 import { COLORS } from "@const/colors.ts";
-import { main } from "@utils/log.ts";
 
 export default addSlashCommand({
   name: "ping",
   description: "Show bot ping.",
   nameLocalizations: { "zh-TW": "顯示延遲" },
   descriptionLocalizations: { "zh-TW": "顯示機器人的延遲" },
-  run: async (bot, interaction) => {
-    return await bot.helpers.editOriginalInteractionResponse(
-      interaction.token,
-      {
-        embeds: new CCOSVEmbed().setTitle("\`🏓 Pong!\`").setDesc(
-          `⏱️: ${Date.now() - snowflakeToTimestamp(interaction.id)}ms\n`,
-        ).setColor(COLORS.BLUE).build(),
-      },
-    ).catch((err) => main.error(err));
+  acknowledge: true,
+  run: async (_, interaction) => {
+    return await interaction.editReply({
+      embeds: new CCOSVEmbed().setTitle("\`🏓 Pong!\`").setDesc(
+        `⏱️: ${Date.now() - snowflakeToTimestamp(interaction.id)}ms\n`,
+      ).setColor(COLORS.ORANGE).build(),
+    });
   },
 });
